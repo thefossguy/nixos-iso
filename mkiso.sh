8954f529fffc500e16e9d08fb4a3c2a9df3099d8
Pratham Patel <thefirst1322@gmail.com>
2023-08-26 09:42:22 +0530

    use nix-output-monitor (nom) to monitor the build in a "pretty" way

--- mkiso.sh
+++ mkiso.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env dash
+#!/usr/bin/env nix-shell
+#!nix-shell -i dash --packages dash nix nix-output-monitor
 
-nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix
+nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix --log-format internal-json -v 2>&1 | nom --json
