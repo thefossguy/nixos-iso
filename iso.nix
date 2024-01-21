65d85ca2592ac2129519d55c80b104fc59198915
Pratham Patel <thefirst1322@gmail.com>
2024-01-21 12:05:41 +0530

    moar trim

--- iso.nix
+++ iso.nix
@@ -8,10 +8,6 @@
   boot = {
     # use the latest Linux kernel
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelParams = [
-      "copytoram"
-      "nomodeset"
-    ];
     # Needed for https://github.com/NixOS/nixpkgs/issues/58959
     supportedFilesystems = lib.mkForce [
       "ext4"
@@ -19,21 +15,6 @@
       "vfat"
       "xfs"
     ];
-  };
-
-  networking = {
-    networkmanager.enable = true;
-    wireless.enable = false;
-    nameservers = [
-      "1.1.1.1"
-      "1.0.0.1"
-      "8.8.8.8"
-      "8.8.4.4"
-    ];
-    firewall = {
-      enable = true;
-      allowPing = true;
-    };
   };
 
   time = {
