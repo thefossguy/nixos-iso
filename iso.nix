0abab637d193f95c8fe5ade0e8d0dcbdca79bd52
Pratham Patel <thefirst1322@gmail.com>
2024-01-21 12:07:51 +0530

    nice comments

--- iso.nix
+++ iso.nix
@@ -6,9 +6,11 @@
   ];
 
   boot = {
-    # use the latest Linux kernel
     kernelPackages = pkgs.linuxPackages_latest;
-    # Needed for https://github.com/NixOS/nixpkgs/issues/58959
+    # since the latest kernel package is installed, there will be a ZFS conflict because
+    # 1. ZFS is developed out of tree and needs to catch up to the latest release
+    # 2. NixOS has ZFS enabled as a default
+    # so force a list of filesystems which I use; sans-ZFS
     supportedFilesystems = lib.mkForce [
       "ext4"
       "f2fs"
