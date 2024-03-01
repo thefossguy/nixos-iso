4f398176b6b98b74e2efd4e780fbb2da76d0ca37
Pratham Patel <thefirst1322@gmail.com>
2024-03-01 15:36:43 +0530

    update mkiso.sh to build with nom and hash ISO

--- mkiso.sh
+++ mkiso.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i dash --packages dash nix nix-output-monitor
 
-nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix --log-format internal-json -v 2>&1 | nom --json
+set -xeu
+
+nom build .
+
+for resultISO in $(basename result/iso/nixos-*-"$(uname -m)"-linux.iso); do
+    cp result/iso/"${resultISO}" .
+    sha512sum "${resultISO}" | awk '{print $1}' | tee "${resultISO}.sha512"
+    sha256sum "${resultISO}" | awk '{print $1}' | tee "${resultISO}.sha256"
+    chown "${USER}:${USER}" "${resultISO}"
+    chmod 644 "${resultISO}"
+done
