66a1a9acf5c55e858544c70291f21283bd429dec
Pratham Patel <thefirst1322@gmail.com>
2024-03-02 03:40:05 +0530

    aint nobody needs sha256sum

--- mkiso.sh
+++ mkiso.sh
@@ -13,7 +13,6 @@ nom build .
 for resultISO in $(basename result/iso/nixos-*-"$(uname -m)"-linux.iso); do
     cp result/iso/"${resultISO}" .
     sha512sum "${resultISO}" | awk '{print $1}' | tee "${resultISO}.sha512"
-    sha256sum "${resultISO}" | awk '{print $1}' | tee "${resultISO}.sha256"
     chown "${USER}:${USER}" "${resultISO}"
     chmod 644 "${resultISO}"
 done
